e34fd67ff76eb658eb8d62ca3ff405c650d7b4b3
jerry73204 <jerry73204@gmail.com>
2020-02-28 02:35:29 +0800

    Create example

--- examples/move_square.rs
+++ examples/move_square.rs
@@ -1,18 +1,3 @@
-# dobot-rust
-
-Drive Dobot robot arms in Rust with high-level and asynchronous API.
-
-## Usage
-
-To include this crate in your project, add this line to your `Cargo.toml`.
-
-```toml
-dobot = "^0.1.1"
-```
-
-## Example
-
-```rust
 use dobot::Dobot;
 use failure::Fallible;
 
@@ -39,8 +24,3 @@ async fn main() -> Fallible<()> {
 
     Ok(())
 }
-```
-
-## License
-
-MIT license. See [license file](LICENSE).
